7ab8e5afb504f7debd10d1e26ca7503d49c6f591
Yan Sun <tsun812@gmail.com>
2022-07-11 11:32:10 -0700

    add input type, extends BaseEntity for Model

--- src/entity/Channel.ts
+++ src/entity/Channel.ts
@@ -1,10 +1,14 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
+import { Field, Int, ObjectType } from "type-graphql";
 
 @Entity()
-export class Channel {
+@ObjectType()
+export class Channel extends BaseEntity {
+  @Field(() => Int)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field()
   @Column({ default: "default channel", nullable: true })
   name: string;
 }
