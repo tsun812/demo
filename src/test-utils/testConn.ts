f93796d8968c84655170589e52033301841f883e
Yan Sun <tsun812@gmail.com>
2022-07-12 09:49:32 -0700

    modify test config

--- src/test-utils/testConn.ts
+++ src/test-utils/testConn.ts
@@ -3,16 +3,10 @@ export const testConn = (drop: boolean = false) => {
   return createConnection({
     name: "test",
     type: "sqlite",
-    database: "database.sqlite",
+    database: ":memory:",
     synchronize: drop,
     dropSchema: drop,
-    entities: ["src/entity/**/*.ts"],
-    migrations: ["src/migration/**/*.ts"],
-    subscribers: ["src/subscriber/**/*.ts"],
-    cli: {
-      entitiesDir: "src/entity",
-      migrationsDir: "src/migration",
-      subscribersDir: "src/subscriber",
-    },
+    entities: ["../entity/**/*.ts"],
+    migrations: ["../migration/**/*.ts"],
   });
 };
